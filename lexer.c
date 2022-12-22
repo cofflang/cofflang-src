bbe3c7887999cc5cc5e0caee98504e39eaf31c33
Isak Horvath <48546805+tavro@users.noreply.github.com>
2022-12-22 17:47:42 +0100

    NEW: Improve dir structure, Make parser read from lexer output

--- lexer.c
+++ lexer.c
@@ -4,6 +4,7 @@ Token get_next_token() {
   Token token;
   token.string = NULL;
   token.ival = 0;
+  token.rval = 0.000;
   
   // Skip whitespace
   while (isspace(text[pos])) {
@@ -87,6 +88,7 @@ Token get_next_token() {
 
     if(token.type == T_INTNUM) {
       token.ival = atoi(token.string);
+      token.rval = (float)token.ival;
     }
     else if(token.type == T_REALNUM) {
       token.rval = atof(token.string);
@@ -152,6 +154,26 @@ void print_symbol_table(Symbol *symbol_table, int num_symbols) {
   }
 }
 
+void write_symbol_table(Symbol *symbol_table, int num_symbols) {
+  FILE *fp = fopen("output/lexer-symtab-out.txt", "w");
+  if (fp == NULL) {
+    perror("Error opening file");
+    return;
+  }
+
+  fprintf(fp, "=============================\n");
+  fprintf(fp, "Symbol Table\n");
+  fprintf(fp, "=============================\n");
+  fprintf(fp, "name        type        value\n");
+  fprintf(fp, "-----------------------------\n");
+  for (int i = 0; i < num_symbols; i++) {
+    Symbol symbol = symbol_table[i];
+    fprintf(fp, "%-12s%-12s%d\n", symbol.name, token_to_string(symbol.type), symbol.value);
+  }
+
+  fclose(fp);
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: %s <filename>\n", argv[0]);
@@ -173,13 +195,23 @@ int main(int argc, char **argv) {
 
   fclose(file);
 
+  FILE *fp = fopen("output/lexer-tokens-out.txt", "w");
+  if (fp == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+
   Token token = get_next_token();
   while (token.type != T_EOF) {
-    printf("Token(%-2d, %-2d): type=%-12s ival=%-4d rval=%-12f string=%-12s\n", token.col, token.row, token_to_string(token.type), token.ival, token.rval, token.string);
+    printf("Token(%-2d, %-2d): type=%-3d(%-12s) ival=%-4d rval=%-12f string=%-12s\n", token.col, token.row, token.type, token_to_string(token.type), token.ival, token.rval, token.string);
+    fprintf(fp, "Token(%-2d, %-2d): type=%-3d(%-12s) ival=%-4d rval=%-12f string=%-12s\n", token.col, token.row, token.type, token_to_string(token.type), token.ival, token.rval, token.string);
     token = get_next_token();
   }
 
+  fclose(fp);
+
   print_symbol_table(symbol_table, num_symbols);
+  write_symbol_table(symbol_table, num_symbols);
 
   return 0;
 }
